d7c66eeb4af8d2bdee25db67e6d559083b1cba2b
LeeHyunWon <qpzmal100@naver.com>
2024-01-02 17:07:06 +0900

    진료비 리스트와 찾기 구현, 가격 통계 google chart  추가

--- animul/src/main/java/com/multi/animul/diagnosis/Receipt_itemService.java
+++ animul/src/main/java/com/multi/animul/diagnosis/Receipt_itemService.java
@@ -1,5 +1,7 @@
 package com.multi.animul.diagnosis;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,16 @@ public class Receipt_itemService {
 	
 	@Autowired
 	Receipt_itemDAO receipt_itemDAO;
+
+	public int insert(Receipt_itemVO receipt_itemVO) {
+		return receipt_itemDAO.insert(receipt_itemVO);
+	}
+	
+	public Receipt_itemVO one(Receipt_itemVO receipt_itemVO) {
+		return receipt_itemDAO.one(receipt_itemVO);
+	}
+	
+	public List<Receipt_itemVO> list() {
+		return receipt_itemDAO.list();
+	}
 }
---
+++ animul/src/main/webapp/WEB-INF/views/diagnosis/diagnosisFind.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:forEach var="x" items="${diagnosisFind}">
+- ${x.diagnosis_name} <br>
+</c:forEach>
