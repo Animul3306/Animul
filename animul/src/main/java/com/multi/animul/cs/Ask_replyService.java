b672543af410eb69b8efa2dd3ee597d9096bdc06
KimMinGyu <forworkinprogress22@gmail.com>
2024-01-09 10:24:35 +0900

    add service

--- animul/src/main/java/com/multi/animul/cs/Ask_replyService.java
+++ animul/src/main/java/com/multi/animul/cs/Ask_replyService.java
@@ -1,5 +1,15 @@
 package com.multi.animul.cs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Ask_replyService {
 
+	@Autowired
+	Ask_replyDAO dao;
+	
+	public int insert(Ask_replyVO vo) {
+		return dao.insert(vo);
+	}
 }
