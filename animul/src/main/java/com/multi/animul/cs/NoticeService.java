0a8a65b2b3672e1062a02e63465d8accfb9f1fc4
KimMinGyu <forworkinprogress22@gmail.com>
2024-01-09 11:55:08 +0900

    cs add notice service, edit askMapper

---
+++ animul/src/main/java/com/multi/animul/cs/NoticeService.java
@@ -0,0 +1,15 @@
+package com.multi.animul.cs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+
+	@Autowired
+	NoticeDAO dao;
+	
+	public int insert(NoticeVO vo) {
+		return dao.insert(vo);
+	}
+}
