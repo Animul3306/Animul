a74cf583d9d7117c15dad2e5d71973baa3ca662d
ChoiSungJoon <140306107+novdev19@users.noreply.github.com>
2024-01-19 19:15:19 +0900

    ver  0.81

--- animul/src/main/java/com/multi/animul/map/HospitalService.java
+++ animul/src/main/java/com/multi/animul/map/HospitalService.java
@@ -36,12 +36,30 @@ public class HospitalService {
 		return one1;
 	}
 	
-	public int count() {
-		return dao.count();
+	public int count(PageVO vo) {
+		return dao.count(vo);
+	}
+
+	public int countAddress(PageVO vo) {
+		return dao.countAddress(vo);
+	}
+
+	public int countKeyword(PageVO vo) {
+		return dao.countKeyword(vo);
 	}
 	
 	public List<HospitalVO> list1(PageVO vo) {
 		List<HospitalVO> list1 = dao.list1(vo);
 		return list1;
+	}
+	
+	public List<HospitalVO> list2(PageVO vo) {
+		List<HospitalVO> list2 = dao.list2(vo);
+		return list2;
+	}
+	
+	public List<HospitalVO> list3(PageVO vo) {
+		List<HospitalVO> list3 = dao.list3(vo);
+		return list3;
 	}
 }
